9bcbe6d2a4589902ee7c92ed4592970c07b60d2e
ROd Zaccara <rodrigo.zaccara@vtex.com.br>
2024-01-12 10:03:14 -0300

    feat: improve dark mode support for stairs chart

--- assets/dark-mode.js
+++ assets/dark-mode.js
@@ -7,15 +7,8 @@ const updateElement = (element, dark) => {
 }
 
 const updateStyles = () => {
-    const iframe = document.getElementById('stairs-chart')
-    const images = document.querySelectorAll('.md-content p img')
     const dark = getComputedStyle(document.body).backgroundColor === 'rgb(255, 255, 255)'
-    if (iframe !== null) {
-        updateElement(iframe, dark)
-    }
-    if (images !== null) {
-        images.forEach(image => updateElement(image, dark))
-    }
+    document.querySelectorAll('.md-content p img').forEach(image => updateElement(image, dark))
 }
 
 document.querySelectorAll('form input.md-option').forEach((value) => {
